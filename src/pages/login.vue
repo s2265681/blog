0f708d024cb5e96c22a419aed45c449ef634a464
商佳伟 <s2265681@163.com>
2019-06-19 10:38:12 +0800

    wxlogin

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,7 @@
 
 <template>
   <div class="login">
-    <img src="https://www.rockshang.cn/sea.jpg" class="header" alt>
+    <img src="https://blog.rockshang.cn/sea.jpg" class="header" alt>
     <span class="logintxt">
       <span @click="toRegiter">注册</span>|
       <span @click="wxLogin">微信登录</span>
